82f25ae3d42cc10adc42a4a822120a7fd8b2ae52
Rami-Pastrami <rami@ramipastrami.engineering>
2023-04-29 16:26:32 -0400

    Added Access to test world / script stuff stuff

---
+++ Assets/Rami-Pastrami/Example_Worlds/Rami_Utils/test.cs
@@ -0,0 +1,15 @@
+﻿
+using UdonSharp;
+using UnityEngine;
+using VRC.SDKBase;
+using VRC.Udon;
+using Rami;
+
+public class test : UdonSharpBehaviour
+{
+    void Start()
+    {
+        float[] a = {1.0f, 2.0f, 3.0f};
+        Debug.Log(Rami_Utils.Array2CSV(a));
+    }
+}
